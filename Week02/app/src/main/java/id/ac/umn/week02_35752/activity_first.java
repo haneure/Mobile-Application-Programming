ddeaff44f4752f10eb8830b6f1736b82da98d6c7
Christian Halim <chris.richard.halim@gmail.com>
2021-08-27 10:05:01 +0700

    Week2 Done

---
+++ Week02/app/src/main/java/id/ac/umn/week02_35752/activity_first.java
@@ -0,0 +1,13 @@
+package id.ac.umn.week02_35752;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_first extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+    }
+}
