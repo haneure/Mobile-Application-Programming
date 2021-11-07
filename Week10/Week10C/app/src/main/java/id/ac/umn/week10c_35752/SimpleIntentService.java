3d64d6e5b1793fb2eb67635cf9e4ac43576f9cb9
Christian Halim <chris.richard.halim@gmail.com>
2021-11-07 18:04:47 +0700

    Week10-Done

---
+++ Week10/Week10C/app/src/main/java/id/ac/umn/week10c_35752/SimpleIntentService.java
@@ -0,0 +1,27 @@
+package id.ac.umn.week10c_35752;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class SimpleIntentService extends IntentService {
+    public SimpleIntentService() {
+        super("SimpleIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Log.i("INTENTSERVICE", "onHandleIntent: IntentService dimulai !");
+        int n = (int) (Math.random()*50)+10;
+        try {
+            for(int i = 0; i < n; i++) {
+                Thread.sleep(200);
+                Log.i("INTENTSERVICE", "onHandleIntent: berjalan " + ((int) ((100 * i) / (float) n)) + " persen");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
